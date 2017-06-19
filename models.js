d9aae9c3a9371a58cf3be697f2269c797284586a
triley22 <triley09@gmail.com>
2017-06-19 10:00:14 -0700

    server.js

--- models.js
+++ models.js
@@ -1,15 +1,5 @@
 const uuid = require('uuid');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
-
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
-
-
 function StorageException(message) {
    this.message = message;
    this.name = "StorageException";
--- server.js
+++ server.js
@@ -1,2 +1,75 @@
 const express = require('express');
 const router = express.Router();
+// const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const {BlogPosts} = require('./models');
+const app = express();
+
+// app.use(morgan('common'));
+
+// BlogPosts.create('title', "JK Rowling");
+
+//blog posts consist of title, content, author name, publication publishDate
+BlogPosts.create('title: Helmut Kohl, Dies at 87', 'article', 'Craig Whitney', 'eventImage: 6/16/2017');
+
+
+app.get('/', (req, res) => {
+	res.json(BlogPosts.get());
+});
+
+app.get('/blogPosts', jsonParser, (req, res) => {
+	const requiredFields = ['name', 'budget'];
+	for (let i=0; i<requiredFields.length; i++) {
+		const field = requiredFields[i];
+		if (!(field in req.body)) {
+			const message = `Missing \`${field}\` in request body`
+			console.error(message);
+			return res.status(400).send(message);
+		}
+	}
+
+	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+	res.status(201).json(item);
+});
+
+app.put('/BlogPosts/:id', jsonParser, (req, res) => {
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating Blog\`${req.params.id}\``);
+  BlogPosts.uppublishDate({
+    title: req.params.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
+  });
+  res.status(204).end();
+});
+
+app.delete('/BlogPosts/:title', (req, res) => {
+	BlogPosts.delete(req.params.title);
+	console.log(`Deleted Blog Post \`${req.params.title}\``);
+	res.status(204).end();
+});
+
+
+
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
