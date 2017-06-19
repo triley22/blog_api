64886882a1b024caacfe4b15e3210fd2cd425ac8
triley22 <triley09@gmail.com>
2017-06-19 10:53:39 -0700

    Get, Post, Put, Delete

--- server.js
+++ server.js
@@ -1,45 +1,56 @@
 const express = require('express');
 const router = express.Router();
-// const morgan = require('morgan');
+
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 const app = express();
 
-// app.use(morgan('common'));
-
-// BlogPosts.create('title', "JK Rowling");
+function post() {
+  return 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc.'
+}
 
 //blog posts consist of title, content, author name, publication publishDate
-BlogPosts.create('title: Helmut Kohl, Dies at 87', 'article', 'Craig Whitney', 'eventImage: 6/16/2017');
+BlogPosts.create('Helmut Kohle', 'Dies at 87', 'Craig Whitney', post(), 'publishDate: 6/16/2017');
+
+BlogPosts.create('London under siege', post(), 'publishDate: 6/19/2017');
 
 
+//add endpoints for GET, it should call `BlogPosts.get()`
+//return JSON objects of blog posts, send back JSON representations of blog posts on get requests to root
 app.get('/', (req, res) => {
-	res.json(BlogPosts.get());
+  res.json(BlogPosts.get());
 });
 
-app.get('/blogPosts', jsonParser, (req, res) => {
-	const requiredFields = ['name', 'budget'];
-	for (let i=0; i<requiredFields.length; i++) {
-		const field = requiredFields[i];
-		if (!(field in req.body)) {
-			const message = `Missing \`${field}\` in request body`
-			console.error(message);
-			return res.status(400).send(message);
-		}
-	}
-
-	const item = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
-	res.status(201).json(item);
-});
-
-app.put('/BlogPosts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
-  for (let i=0; i<requiredFields.length; i++) {
+//add endpoints for POST request
+//return JSON object representing the new post including the id, which blogPosts will create
+//send 400 error if post doesn't contain `title`, `content`, `author`
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
       const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = BlogPosts.create(
+    req.body.title, req.body.content, req.body.author);
+  res.status(201).json(item);
+});
+
+
+//add endpoint for PUT requests to update BlogPosts
+//call BlogPosts.update(), match id, required fields: id, title, content, author, publish date
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = [
+  'id', 'title', 'content', 'author', 'publishDate'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`$(field)\` in request body`
       console.error(message);
       return res.status(400).send(message);
     }
@@ -51,9 +62,10 @@ app.put('/BlogPosts/:id', jsonParser, (req, res) => {
     console.error(message);
     return res.status(400).send(message);
   }
-  console.log(`Updating Blog\`${req.params.id}\``);
-  BlogPosts.uppublishDate({
-    title: req.params.title,
+  console.log(`Updating blog post with id \`${req.params.id}\``);
+  const updatedItem = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
     content: req.body.content,
     author: req.body.author,
     publishDate: req.body.publishDate
@@ -61,14 +73,13 @@ app.put('/BlogPosts/:id', jsonParser, (req, res) => {
   res.status(204).end();
 });
 
-app.delete('/BlogPosts/:title', (req, res) => {
-	BlogPosts.delete(req.params.title);
-	console.log(`Deleted Blog Post \`${req.params.title}\``);
-	res.status(204).end();
+
+//endpoint for delete, ud as url path variable and call
+app.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post with id \`${req.params.ID}\``);
+  res.status(204).end();
 });
-
-
-
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
